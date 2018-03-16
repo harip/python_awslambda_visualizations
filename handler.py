caf777211416774add331681c001e9767a3f5dd1
Hari Pulugurta <charanp@gmail.com>
2018-03-16 02:24:29 -0400

    wip - aws lambda to generate matplotlib

---
+++ handler.py
@@ -0,0 +1,28 @@
+# handler.py
+import json
+import pandas as pd
+import matplotlib.pyplot as plt, mpld3
+
+def main(event, context):
+    PERF_DATA = pd.read_csv('https://res.cloudinary.com/harip/raw/upload/v1520830685/timeseries.csv')
+
+    # Read column names and ignore Group column
+    COL_NAMES = [colname for colname in PERF_DATA.columns.values if "Age_" in colname]
+
+    # get average values for each column as a series
+    AVG_VALUES = pd.Series([PERF_DATA[col_name].mean() for col_name in COL_NAMES])
+
+
+    FIG, AX = plt.subplots(nrows=1, ncols=1)
+    AVG = AX.twiny()
+
+    # plot series
+    AVG_VALUES.plot(kind='line', grid=True, ylim={0, 100})
+    HTML=mpld3.fig_to_html(FIG)
+    return {
+        'statusCode': 200,
+        'body': HTML
+    }
+
+if __name__ == "__main__":
+    main('', '')
